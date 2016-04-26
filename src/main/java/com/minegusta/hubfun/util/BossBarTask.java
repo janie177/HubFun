eef5f0288742eee3d7f3d73c89555bcf90cdecfa
Jan <janie177@hotmail.com>
2016-04-26 19:18:30 +0200

    Fixed faulty config.

--- src/main/java/com/minegusta/hubfun/util/BossBarTask.java
+++ src/main/java/com/minegusta/hubfun/util/BossBarTask.java
@@ -12,9 +12,7 @@ import java.util.List;
 public class BossBarTask {
 	private static int ID = -1;
 
-	private static final List<BossBarMessage> messages = Lists.newArrayList(
-
-	);
+	private static final List<BossBarMessage> messages = Lists.newArrayList();
 
 	public static void init(FileConfiguration conf) {
 		for (String path : conf.getConfigurationSection("messages").getKeys(false)) {
@@ -22,28 +20,26 @@ public class BossBarTask {
 				String message = conf.getString("messages." + path + ".message");
 				BossBarAPI.Color color = BossBarAPI.Color.valueOf(conf.getString("messages." + path + ".color"));
 				BossBarAPI.Style style = BossBarAPI.Style.valueOf(conf.getString("messages." + path + ".style"));
-
 				messages.add(new BossBarMessage(message, color, style));
 			} catch (Exception ignored) {
+				System.out.println("Error while trying to read a config entry in HubFun.");
 			}
 		}
 	}
 
 	private static int currentId = 0;
-	private static BossBarMessage current = messages.get(0);
 
 	public static void start() {
 		ID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.PLUGIN, new Runnable() {
 			@Override
 			public void run() {
-
+				BossBarMessage current = messages.get(currentId);
 				Bukkit.getOnlinePlayers().stream().forEach(player -> BossBarAPI.addBar(player, new TextComponent(current.getMessage().replace("%name%", player.getName())), current.getColor(), current.getStyle(), 1.0f, 40, 2));
 
 				currentId++;
 				if (currentId >= messages.size()) {
 					currentId = 0;
 				}
-				current = messages.get(currentId);
 			}
 		}, 80, 80);
 	}
